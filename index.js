b4bc7b9efb85774e2ca066a1b47febb9248b16bc
Maks <maksym.shershen@gmail.com>
2023-01-19 16:42:31 +0100

    add functionality upload images

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
-
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
-
 const multer = require('multer')
+const path=require('path')
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,useUnifiedTopology:true}, () => {
     console.log("connect mongodb")
 })
+
+app.use('/images',express.static(path.join(__dirname,"public/images")))
+
 
 //middleware
 app.use(express.json())
@@ -23,15 +25,28 @@ app.use(helmet())
 app.use(morgan('common'))
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "/public/images")
+    destination: function(req, file, cb){
+        cb(null, "public/images")
     },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname)
+    filename: function(req, file, cb){
+        cb(null, file.originalname)
     }
 })
 
-const upload = multer({storage})
+
+const types = ['public/images/png', 'public/images/jpeg', 'public/images/jpg']
+
+const filterFile = (req, file, cb) => {
+    if(types.includes(file.mimetype)) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+
+const upload = multer({storage,filterFile})
+
 app.post('/api/upload', upload.single('file'), (req, res) => {
     try{
         return res.status(200).json('File uploaded successfully')
